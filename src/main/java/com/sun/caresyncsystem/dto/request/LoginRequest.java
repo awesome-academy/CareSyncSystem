f3199d5a44dba4e83efe89b0dda894c3319ee84f
minhpv-3059 <phan.van.minh@sun-asterisk.com>
2025-06-12 11:03:38 +0700

    feat: add api login

---
+++ src/main/java/com/sun/caresyncsystem/dto/request/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.sun.caresyncsystem.dto.request;
+
+public record LoginRequest(String email, String password) {
+}
--- src/main/java/com/sun/caresyncsystem/service/AuthenticationService.java
+++ src/main/java/com/sun/caresyncsystem/service/AuthenticationService.java
@@ -1,5 +1,9 @@
 package com.sun.caresyncsystem.service;
 
+import com.sun.caresyncsystem.dto.request.LoginRequest;
+import com.sun.caresyncsystem.dto.response.LoginResponse;
+
 public interface AuthenticationService {
      void activateAccount(String token);
+     LoginResponse login(LoginRequest request);
 }
