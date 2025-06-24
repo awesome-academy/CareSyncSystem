068e6e868d65940b84e8d712f5362712afb2190b
nampx-2246 <pham.xuan.nam@sun-asterisk.com>
2025-06-24 15:39:37 +0700

    feat: booking history

--- src/main/java/com/sun/caresyncsystem/utils/api/BookingApiPaths.java
+++ src/main/java/com/sun/caresyncsystem/utils/api/BookingApiPaths.java
@@ -1,7 +1,7 @@
 package com.sun.caresyncsystem.utils.api;
 
 public class BookingApiPaths {
-    public static final String BASE = "/api/v1/booking";
+    public static final String BASE = "/api/v1/bookings";
 
     public static class Endpoint {
         public static final String RESCHEDULE = "/{id}/reschedule";
